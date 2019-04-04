bfd259029ad1f972691b118aee77d10fabd2f9fd
virginia koar <virginiakoar@TimKoar.fios-router.home>
2019-04-04 16:37:47 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,27 +1,27 @@
 katz_deli = []
+
 def line(katz_deli)
   if katz_deli.length == 0
-    puts "The line is currently empty."
+    puts  "The line is currently empty."
   else
     current_line = "The line is currently:"
-    katz_deli.each_with_index do |name, index|
-    current_line += " #{index.to_i + 1}. #{name}"
+    katz_deli.map.with_index do |name, i|
+    current_line  << " #{i+1}. #{name}"
     end
-    puts "#{current_line}"
+    puts current_line
   end
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
-  line_place = katz_deli.count
-  if line_place == 0
+  if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli.first}."
-    katz_deli.shift
-end
+  next_customer = katz_deli.shift
+  puts "Currently serving #{next_customer}."
+  end
 end
